f46e775bebd27ceff64bcc2b17adce5628919ed1
Yaser <kalkanyaser@gmail.com>
2024-04-03 22:37:37 +0300

    soru3 ve soru4

---
+++ soru2/birimler_veri_ekleme.sql
@@ -0,0 +1,6 @@
+USE foy3;
+
+INSERT INTO birimler (birim_id, birim_ad) VALUES
+(1, 'Yazılım'),
+(2, 'Donanım'),
+(3, 'Güvenlik');
---
+++ soru3/birim_listeleme.sql
@@ -0,0 +1,5 @@
+USE foy3;
+
+SELECT ad, soyad, maas
+FROM calisanlar
+WHERE calisan_birim_id IN (1, 2);
