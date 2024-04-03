577bdf45f47f77f1c2625e7be253220395e6c6af
Yaser <kalkanyaser@gmail.com>
2024-04-03 22:33:01 +0300

    soru2

---
+++ soru2/unvan_veri_ekleme.sql
@@ -0,0 +1,11 @@
+USE foy3;
+
+INSERT INTO unvan (unvan_calisan_id, unvan_calisan, unvan_tarih) VALUES
+(1, 'Yönetici', '2016-02-20 00:00:00.000'),
+(2, 'Personel', '2016-06-11 00:00:00.000'),
+(3, 'Personel', '2016-06-11 00:00:00.000'),
+(4, 'Müdür', '2016-06-11 00:00:00.000'),
+(5, 'Yönetici Yardımcısı', '2016-06-11 00:00:00.000'),
+(6, 'Personel', '2016-06-11 00:00:00.000'),
+(7, 'Takım Lideri', '2016-06-11 00:00:00.000'),
+(8, 'Takım Lideri', '2016-06-11 00:00:00.000');
